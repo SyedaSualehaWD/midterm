17904edb57d3c49dd0991d3736036f9bd1223aaf
SyedaSualehaWD <syedasualehawd@gmail.com>
2025-04-24 18:02:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function number(num1 , num2 , operator){
+    if(operator==="+"){
+        return num1 + num2;
+    }
+    else if(operator==="*"){
+        return num1 * num2
+    }
+    else("your answer")
+}
+document.body.innerHTML=(number(44,55,"*"));
